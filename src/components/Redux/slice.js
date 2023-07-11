52c5b952632f46393facfb708d34f696ae1876fb
Khushboo Yadav <Khushbooyadav1091@gmail.com>
2023-07-11 11:44:43 +0530

    updated

--- src/components/Redux/slice.js
+++ src/components/Redux/slice.js
@@ -1,43 +1,44 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-export const getData = createAsyncThunk('TableData/getData', async () => {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    return response.data;
-  } catch (error) {
-    throw Error(error);
-  }
-});
-const customSlice=createSlice({
-    name:"TableData",        
-    initialState:{
-        data:[],
-        loading:false,
-        alerts:{
-        alertSuccess:'',
-        alertFailure:'', 
-        }
-    },
-    // reducer:{
-    //   dismissAlert(state){
-    //       state.alerts=false;
-    //   }
-    // },
-    extraReducers:{
-[getData.pending]:(state)=>{
-  state.loading=true;
-},
-[getData.fulfilled]:(state,action)=>{
-  state.loading=false;
-  state.data=action.payload;
-  state.alerts.alertSuccess="Data fetched"
-},
-[getData.rejected]:(state)=>{
-  state.loading=false; 
-  state.alerts.alertFailure="rejected"
-}
-    }
-})
+export const getData = createAsyncThunk(
+  "TableData/getData",
+  async (args, { rejectWithValue }) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-// export const {dismissAlert}=customSlice.actions;
+    try {
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return rejectWithValue("opps found an error");
+    }
+  }
+);
+
+const customSlice = createSlice({
+  name: "TableData",
+  initialState: {
+    data: [],
+    loading: false,
+    alerts: {
+      alertSuccess: "",
+      alertFailure: "",
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getData.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getData.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.alerts.alertSuccess = "Data fetched";
+    });
+    builder.addCase(getData.rejected, (state) => {
+      state.loading = false;
+      state.alerts.alertFailure = "rejected";
+    });
+  },
+});
+
+
 export default customSlice.reducer;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,66 +1,69 @@
 import React, { useEffect } from "react";
 import { dismissAlert, getData } from "../components/Redux/slice";
 import DataTable from "react-data-table-component";
-import { useDispatch, useSelector } from 'react-redux'
-import {toast,ToastContainer} from 'react-toastify'
-
-import Loader from './Loader'
+import { useDispatch, useSelector } from "react-redux";
+import { toast, ToastContainer } from "react-toastify";
+import Typography from "@mui/material/Typography";
+import Loader from "./Loader";
+import 'react-toastify/dist/ReactToastify.css';
 const columns = [
   {
-      name: 'Id',
-      selector: row => row.id ,
+    name: "Id",
+    selector: (row) => row.id,
   },
-    {
-        name: 'Body',
-        selector: row => row.body,
-    },
   {
-      name: 'userId',
-      selector: row => row.userId,
+    name: "Body",
+    selector: (row) => row.body,
+  },
+  {
+    name: "userId",
+    selector: (row) => row.userId,
   },
 ];
 const customStyles = {
   cells: {
-      style: {
-          width: '70px', 
-      },}
-    }
-
+    style: {
+      width: "70px",
+    },
+  },
+};
 
 const Tables = () => {
-  const dispatch=useDispatch();
-  const { loading, data ,alerts} = useSelector((state) => state.TableData);
+  const dispatch = useDispatch();
+  const {loading, data, alerts } = useSelector((state) => state.TableData);
+  // console.log(loading,"loadingloadingloadingloading")
+
+  useEffect(() => {
+    dispatch(getData())
+    .then(()=>(
+    toast.success(alerts.alertSuccess)))
+    // .then((res)=>{
+    //   console.log(res)
+
+    //   console.log("alerts",alerts)
+
+    // })
+    .catch(()=>toast.error(alerts.alertFailure))
+  }, []);
   
 
-  // useEffect(()=>{
-  //   dispatch(getData())
-  //   .then(()=>(toast(alerts.alertSuccess)))
-  //   .catch(()=>toast(alerts.alertFailure))
-  // },[])
-  useEffect(()=>{
-    dispatch(getData()).then(setTimeout(()=>(alert(alerts.alertSuccess)),2000))
-    .catch(console.log(alerts.alertFailure))
-  },[])
-  
-  
   return (
     <div>
+      <Typography
+        variant="h5"
+        sx={{ display: "flex", justifyContent: "center" }}
+        gutterBottom
+      >
+        Data Table
+      </Typography>
       <DataTable
         data={data}
         columns={columns}
         progressPending={loading}
         progressComponent={<Loader />}
         customStyles={customStyles}
-        />  
-        {/* <ToastContainer  position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover/> */}
+      />
+       <ToastContainer/>
     </div>
   );
 };
